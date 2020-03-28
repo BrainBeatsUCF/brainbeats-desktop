3759137ab60dae51de62c4b341721bed9d0cfa72
lloydoad <lloyd.lloyddapaah@gmail.com>
2020-03-28 00:31:51 -0400

    [LD] Add workstation editing beat buttons

--- public/components/Studio/Studio.js
+++ public/components/Studio/Studio.js
@@ -1,7 +1,7 @@
 import "./Studio.css";
 
 import React from "react";
-
+import whitePlayButton from "../../images/whitePlayButton.png";
 import { StudioSection } from "./StudioSection";
 
 export default class Studio extends React.Component {
@@ -34,6 +34,71 @@ export default class Studio extends React.Component {
     console.log("load sample", index);
   };
 
+  handlePlayWorkstation = (editingBeat) => {
+    console.log("play");
+  }
+
+  handleUploadImage = () => {
+    console.log("upload image");
+  }
+
+  handleNameChange = (value) => {
+    console.log(value);
+  }
+
+  handleSaveBeat = (editingBeat) => {
+    console.log("save beat")
+  }
+
+  renderWorkStation = (data, play, nameChange, uploadImage, save) => {
+    return (
+      <div className="StudioWorkstationContent">
+        <div className="StudioSectionHeading">
+          <h4 className="HomeHorizontalScrollMenuTitle">
+            Workstation
+          </h4>
+        </div>
+        <div className="SWContainer">
+          <div className="SWActivators">
+
+          </div>
+          <div className="SWCanvasContainer">
+
+          </div>
+        </div>
+        <div className="SWButtonSection">
+          <div className="SWButtonLeft">
+            <img
+              src={whitePlayButton}
+              className="Studio Button StudioButtonLarge"
+              onClick={() => play(data)}
+            ></img>
+            <input
+              className="SWButton SWButtonGray SWButtonLong"
+              placeholder="Beat Title"
+              type="text"
+              onChange={event => nameChange(event.target.value)}
+            ></input>
+            <input
+              className="SWButton SWButtonGray SWButtonMedium"
+              value="Upload Beat Image"
+              type="button"
+              onClick={() => uploadImage()}
+            ></input>
+          </div>
+          <div className="SWButtonRight">
+            <input
+              className="SWButton SWButtonBlue SWButtonShort"
+              value="Save"
+              type="button"
+              onClick={() => save()}
+            ></input>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="StudioBackground">
@@ -49,7 +114,15 @@ export default class Studio extends React.Component {
           this.handleCreateSample,
           this.handleLoadSample
         )}
-        <div className="StudioWorkstationBackground"></div>
+        <div className="StudioWorkstationBackground">
+          {this.renderWorkStation(
+            {},
+            this.handlePlayWorkstation,
+            this.handleNameChange,
+            this.handleUploadImage,
+            this.handleSaveBeat
+          )}
+        </div>
       </div>
     );
   }
