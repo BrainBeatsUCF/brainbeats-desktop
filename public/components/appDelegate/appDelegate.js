4b327ccb64068a93d60ae0475b87ea254408da4d
Lloyd Dapaah <lloyd.lloyddapaah@gmail.com>
2020-10-30 15:23:04 -0400

    [LD] Remove profile image from public beat card

--- public/components/appDelegate/appDelegate.js
+++ public/components/appDelegate/appDelegate.js
@@ -16,7 +16,7 @@ DefaultGridObject.samples = []
  * }} props
  */
 const AppDelegate = props => {
-  const [navigationPage, setNavigationPage] = useState(NavigationPage.Studio)
+  const [navigationPage, setNavigationPage] = useState(NavigationPage.Home)
   const [isMakingNetworkActivity, setIsMakingNetworkActivity] = useState(false)
   const [currentGridItem, setCurrentGridItem] = useState(DefaultGridObject)
 
